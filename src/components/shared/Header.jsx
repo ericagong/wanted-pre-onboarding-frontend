22626a07ccf2cf19c27e5bad35ed4c89978ab636
ericagong <48196721+ericagong@users.noreply.github.com>
2022-10-12 08:58:05 +0900

    feat: header 추가

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -3,8 +3,8 @@ import styled from "styled-components";
 const Header = (props) => {
   return (
     <StLayout>
-      <div>My Todo List</div>
-      <div>React</div>
+      <StLogo>Simple Todos</StLogo>
+      <StInfo>with React</StInfo>
     </StLayout>
   );
 };
@@ -13,13 +13,38 @@ export default Header;
 
 const StLayout = styled.header`
   width: 100vw;
-  height: 5vh;
+  height: 8vh;
 
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
 
-  margin: 10px 0px;
   padding: 0px 30px;
+
+  background: #ffffff;
+  box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.04);
+`;
+
+const StFont = styled.div`
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 500;
+  letter-spacing: -0.5px;
+`;
+
+const StLogo = styled(StFont)`
+  font-family: "Lobster";
+  font-size: 20px;
+  color: #256ef1;
+
+  padding: 30px 0px;
+`;
+
+const StInfo = styled(StFont)`
+  font-size: 10px;
+  line-height: 14px;
+  color: #656565;
+
+  margin-top: 3px;
 `;
